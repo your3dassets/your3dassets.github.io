ead47e3cb4fe38f800d2d93f4e45d1fcc91f8071
anythinganyoneunknown-lang <anythinganyoneunknown@gmail.com>
2026-03-09 16:46:13 +0530

    suurchbox

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("3D Market Loaded");
+const searchBox = document.getElementById("searchBox");
+const cards = document.querySelectorAll(".card");
+
+searchBox.addEventListener("keyup", function(){
+
+let value = searchBox.value.toLowerCase();
+
+cards.forEach(function(card){
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(value)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
+}
+
+});
+
+});
